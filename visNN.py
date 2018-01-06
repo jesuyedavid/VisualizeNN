1601fb373ba1c89e5961ea189246961434ba9316
unknown <jesuyed@yahoo.com>
2018-01-06 16:37:19 -0500

    interactive and responsible

--- visNN.py
+++ visNN.py
@@ -3,18 +3,23 @@ import random
 import math
 
 
-inputLayerN=int(input("How many neurons in are in the input layer: "))
-noHiddenLayers=int(input("How many hidden layers: "))
-outputLayerN=int(input("How many neurons in output layer: "))
-window=GraphWin("VisualizeNN", 500, 500)
+window=GraphWin("VisualizeNNFullyConnected", 700, 500)
+window.setBackground("white")
+prom=Text(Point(200, 470), "How many neurons in first layer")
+prom.draw(window)
+texE=Entry(Point(200, 490), 20)
+texE.draw(window)
 
-#Build neurons in first layer and line them up
+#click the mouse to signal done entering text
+window.getMouse()
 
+inputLayerN=int(texE.getText())
 
+firLayerC=100
 newrow=100
 for i in range(inputLayerN):
   # make the circle in the center
-  circle = Circle(Point(100, newrow), 20)
+  circle = Circle(Point(firLayerC, newrow), 20)
 
   # set the circle to a random colors
   color = color_rgb(random.randint(0, 255),
@@ -25,5 +30,24 @@ for i in range(inputLayerN):
   circle.draw(window)
   newrow+=70
 
-while True:
-  pass
+print(newrow)
+prom.setText("Neurons in second layer")
+texE.setText("")
+window.getMouse()
+secLayerN=int(texE.getText())
+newrow=100
+secLayerC=firLayerC+100
+for i in range(secLayerN):
+  circle = Circle(Point(secLayerC, newrow), 20)
+
+  # set the circle to a random colors
+  color = color_rgb(random.randint(0, 255),
+                    random.randint(0, 255),
+                    random.randint(0, 255))
+  circle.setFill(color)
+  circle.draw(window)
+  newrow+=70
+
+
+window.getMouse()
+window.close()
